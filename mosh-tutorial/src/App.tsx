077c6b1a20a0ff14101af98f7acbf2231d3ccdc6
Ka Thas <kavinthas@gmail.com>
2024-12-01 14:42:16 +0900

    started advent of code

--- mosh-tutorial/src/App.tsx
+++ mosh-tutorial/src/App.tsx
@@ -1,19 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-    let items = ["London", "Oslo", "Seoul"];
-
-    const handleSelectItem = (item: string) => {
-        console.log(item);
-    };
-
     return (
         <div>
-            <ListGroup
-                items={items}
-                heading="Cities"
-                onSelectItem={handleSelectItem}
-            />
+            <Alert>Alarmalarm</Alert>
         </div>
     );
 }
